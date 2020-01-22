99102d1a1b83fd083c488485db418067e1bd7ef9
gel1993 <59850686+gel1993@users.noreply.github.com>
2020-01-22 18:32:11 -0300

    3

--- fase02/ex03/maiusculo.js
+++ fase02/ex03/maiusculo.js
@@ -1,3 +1,3 @@
-function maiusculo(palavra){
-    return palavra.substring(0,3). toUpperCase()
+function maiusculo(palavra) {
+    return palavra.substring(0,3). toUpperCase( ).concat(palavra.substr(3));
 }
