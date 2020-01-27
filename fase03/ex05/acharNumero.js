11920e3fb48b4d86c03db8599561b990d420d76e
gel1993 <59850686+gel1993@users.noreply.github.com>
2020-01-27 19:34:59 -0300

    5

---
+++ fase03/ex05/acharNumero.js
@@ -0,0 +1,16 @@
+function acharNumero(num){
+    if (num<5)
+    return('mercurio');
+    else if (num <=10 && num>=5)
+    return('venus');
+    else if (num>10 && num<=100)
+    return('terra');
+    else
+    return('marte');
+
+}
+
+
+
+
+    
