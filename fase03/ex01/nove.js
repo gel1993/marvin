ab23a0669c5b85cd0edd4b64d3e5c47c0d2af332
gel1993 <59850686+gel1993@users.noreply.github.com>
2020-01-29 19:47:19 -0300

    1

--- fase03/ex01/nove.js
+++ fase03/ex01/nove.js
@@ -1,8 +1,8 @@
 function nove(num){
-    if ( num.substr(-1) == 9){
-        return ("true");
-}else{
-     return ('false');
-    }
-    
-    }
+ var n1 = num.toString();
+ var n2 = n1.substring(n1.length-1,n1.length);
+ if(n2==="9")
+ return true;
+ else
+ return false;
+}
