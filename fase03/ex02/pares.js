21fae8af2278b3c621dadb9a53f5c3e5931ddc44
gel1993 <59850686+gel1993@users.noreply.github.com>
2020-01-29 19:54:58 -0300

    2

--- fase03/ex02/pares.js
+++ fase03/ex02/pares.js
@@ -1,6 +1,6 @@
 function pares(num1, num2){
     if (num1%2 ===0 && num2%2 ===0)
-        return('Os números são  pares');
+        return('Os números são pares');
     else
        return ('Os números não são pares');
 }
