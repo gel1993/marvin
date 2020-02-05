30e15e4c76c48aa9e68e6ef95f8e6084c2e790fc
gel1993 <59850686+gel1993@users.noreply.github.com>
2020-02-05 19:41:16 -0300

    2

---
+++ fase04/ex02/fizzbuzz.js
@@ -0,0 +1,19 @@
+function fizzbuzz(num1, num2){
+    
+while (num1 <= num2){
+    
+    if(num1%3 === 0 && num1%5 === 0){
+        console.log(num1 + " FizzBuzz")
+    }
+    else if (num1%3 === 0){
+        console.log(num1 + " Fizz")
+    }
+    else if(num1%5 === 0){
+        console.log(num1 + " Buzz")
+    }
+    else
+    console.log(num1)
+  
+num1 ++;
+}
+}
