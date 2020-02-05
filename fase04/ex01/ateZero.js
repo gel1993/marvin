9a0ea702753689192cce92d79c3107bc4e20abc5
gel1993 <59850686+gel1993@users.noreply.github.com>
2020-02-05 18:24:21 -0300

    1

---
+++ fase04/ex01/ateZero.js
@@ -0,0 +1,13 @@
+function ateZero(num){
+    var zero = 0;
+      if (num > 0){
+    while ( zero <= num){
+    console.log (zero);
+    zero++;
+}
+}
+while(num<=0){
+    console.log(num);
+    num++;
+}
+}
